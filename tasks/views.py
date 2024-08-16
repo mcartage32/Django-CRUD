06dd1c43f38d2cd3f28b969c6c155cc8e7c55fe3
Marcelo Cartagena <mcartage33@gmail.com>
2024-08-16 15:57:31 -0500

    Se organizan rutas

--- tasks/views.py
+++ tasks/views.py
@@ -1,13 +1,24 @@
+from django.db import IntegrityError
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
+from django.contrib.auth import login
 
 # Create your views here.
 def index(request):
+    return render(request, 'index.html')
+
+
+def tasks(request):
+    return render(request, 'tasks.html')
+
+
+def signup(request):
     if request.method == "GET":
-        return render(request, "signup.html", {"form": UserCreationForm})
+        return render(request, "signup.html", {
+            "form": UserCreationForm
+        })
     else:
         if request.POST["password1"] == request.POST["password2"]:
             try:
@@ -15,17 +26,16 @@ def index(request):
                     username=request.POST["username"],
                     password=request.POST["password1"],
                 )
+                login(request, user)
                 user.save()
-                return HttpResponse("Usuario creado exitosamente!")
+                return redirect('tasks')
             except:
-                return render(
-                    request,
-                    "signup.html",
-                    {"error": "Error al crear el usuario", "form": UserCreationForm},
-                )
+                return render(request, "signup.html", {
+                    "error": "Error al crear el usuario",
+                    "form": UserCreationForm
+                })
         else:
-            return render(
-                request,
-                "signup.html",
-                {"error": "Las contraseñas no coinciden", "form": UserCreationForm},
-            )
+            return render(request, "signup.html", {
+                "error": "Las contraseñas no coinciden",
+                "form": UserCreationForm
+            })
