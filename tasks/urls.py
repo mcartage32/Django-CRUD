c592ef6b569a9884ceb6a310b94b1c6b07f11603
Marcelo Cartagena <mcartage33@gmail.com>
2024-08-21 07:32:48 -0500

    Ajustes en urls y templates

--- tasks/urls.py
+++ tasks/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('signup', views.signup, name='signup'),
     path('tasks', views.tasks, name='tasks'),
     path('logout', views.signout, name='logout'),
-    path('login', views.signin, name='login'),
+    path('accounts/login/', views.signin, name='login'),
     path('create-task', views.createtask, name='create-task'),
     path('task-detail/<int:task_id>', views.taskdetail, name='task-detail')
 ]
