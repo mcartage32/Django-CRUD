6fba3ddc393890da138d3c5d407583438959344f
Marcelo Cartagena <mcartage33@gmail.com>
2024-08-22 09:41:46 -0500

    Se termina de agregar estilos

--- tasks/forms.py
+++ tasks/forms.py
@@ -7,6 +7,11 @@ class CreateTaskForm(ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'description', 'important']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Write a title'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Write description'}),
+            'datecompleted': forms.DateInput(attrs={'type': 'date'})
+        }
 
 
 class UpdateTaskForm(ModelForm):
